fccd11d7404dcc40c7fd7fc4927c438a65ed9304
Sumit <sumitbaghel22a@gmail.com>
2025-04-13 11:08:33 +0530

    changed2

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // console.log(process.env.FRONTEND_URL)
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL , 'http://localhost:5173'],
     credentials: true,
   })
 );
@@ -42,7 +42,7 @@ app.use("/upload", uploadRoutes);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL ,
+    origin: [process.env.FRONTEND_URL , 'http://localhost:5173'] ,
     credentials: true,
   },
 });
